a17c4fccc6157e7229f9a29f5aca14bb29b9b0e1
David Tejerina <davidtejerina2003@gmail.com>
2024-02-12 11:08:17 +0100

    inicio

---
+++ src/main/java/alex/david/diveHub/DiveHubApplication.java
@@ -0,0 +1,13 @@
+package alex.david.diveHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiveHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiveHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/alex/david/diveHub/DiveHubApplicationTests.java
@@ -0,0 +1,13 @@
+package alex.david.diveHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiveHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
