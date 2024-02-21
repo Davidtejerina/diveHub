8e823ee4ab8ac63b25f1e14d801afe98e3912ce6
Alex <alejandroplozano@gmail.com>
2024-02-21 10:44:12 +0100

    actualizacion 21/02b  22

--- src/main/java/models/Product.java
+++ src/main/java/models/Product.java
@@ -14,6 +14,7 @@ public class Product {
    @Id
    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private Long id2;
     private String name;
     private String description;
     private Double startingPrice;
