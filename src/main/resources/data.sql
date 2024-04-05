54c1f797ac59bede03c645b4701ad85150b174ae
David Tejerina <davidtejerina2003@gmail.com>
2024-04-05 16:24:16 +0200

    datasql casi finalizado

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,38 +1,72 @@
 INSERT INTO product (name, description, starting_price, final_price, image, category, tag)
 VALUES
-    ('Buceo extremo', 'Emocionante experiencia de buceo en aguas profundas', 150.99, 199.99, 'buceo_extremo.jpg', 0, 'aventura'),
-    ('Kit de snorkel', 'Kit completo de snorkel para explorar arrecifes de coral', 49.99, 59.99, 'snorkel.jpg', 2, 'accesorio'),
-    ('Curso de buceo avanzado', 'Curso intensivo para mejorar tus habilidades de buceo', 199.99, 249.99, 'curso_avanzado.jpg', 1, 'formación'),
-    ('Excursión a la barrera de coral', 'Viaje en barco para explorar la impresionante barrera de coral', 99.99, 149.99, 'excursion_coral.jpg', 0, 'naturaleza'),
-    ('Traje de neopreno', 'Traje de neopreno de alta calidad para buceo en aguas frías', 79.99, 99.99, 'neopreno.jpg', 2, 'equipo'),
-    ('Curso de buceo básico', 'Curso introductorio para principiantes en el mundo del buceo', 149.99, 199.99, 'curso_basico.jpg', 1, 'formación'),
-    ('Paquete de fotografía submarina', 'Equipo completo para capturar impresionantes imágenes bajo el agua', 199.99, 249.99, 'fotografia_submarina.jpg', 0, 'foto'),
-    ('Accesorios de buceo', 'Una amplia selección de accesorios para buceadores de todos los niveles', 29.99, 39.99, 'accesorios_buceo.jpg', 2, 'accesorio'),
-    ('Tour nocturno de buceo', 'Experiencia única de buceo nocturno para explorar la vida marina bajo la luz de la luna', 129.99, 179.99, 'buceo_nocturno.jpg', 0, 'aventura'),
-    ('Botellas de oxígeno', 'Botellas de oxígeno de alta presión para buceo seguro y prolongado', 89.99, 109.99, 'botellas_oxigeno.jpg', 2, 'equipo'),
-    ('Curso de rescate acuático', 'Aprende técnicas de rescate y primeros auxilios en el agua', 179.99, 229.99, 'rescate_acuatico.jpg', 1, 'formación'),
-    ('Viaje a islas tropicales', 'Paquete de vacaciones que incluye buceo en aguas cristalinas y playas paradisíacas', 799.99, 999.99, 'islas_tropicales.jpg', 0, 'vacaciones');
+    ('Buceo extremo', 'Emocionante experiencia de buceo en aguas profundas', 150.99, 199.99, 'https://static.nationalgeographic.es/files/styles/image_3200/public/0001-dsc05955.jpg?w=1600&h=900', 0, 'aventura'),
+    ('Kit de snorkel', 'Kit completo de snorkel para explorar arrecifes de coral', 49.99, 59.99, 'https://contents.mediadecathlon.com/p2296924/k$d2b2b93d0f60cc3c0dbf1d8f2e294b51/sq/kit-mascara-tubo-buceo-snorkel-subea-100-adulto-negro.jpg?format=auto&f=800x0', 2, 'accesorio'),
+    ('Curso de buceo avanzado', 'Curso intensivo para mejorar tus habilidades de buceo', 199.99, 249.99, 'https://costaricadivers.com/wp-content/uploads/2022/10/curso-de-buceo-avanzado.jpg', 1, 'formación'),
+    ('Excursión a la barrera de coral', 'Viaje en barco para explorar la impresionante barrera de coral', 99.99, 149.99, 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/2b/ab/56/83/caption.jpg?w=500&h=400&s=1', 0, 'naturaleza'),
+    ('Traje de neopreno', 'Traje de neopreno de alta calidad para buceo en aguas frías', 79.99, 99.99, 'https://www.redwoodpaddle.es/10499-superlarge_default/neopreno-manatee-5-4.jpg', 2, 'equipo'),
+    ('Curso de buceo básico', 'Curso introductorio para principiantes en el mundo del buceo', 149.99, 199.99, 'https://www.grancanariadivers.com/wp-content/uploads/2017/12/courses-1.jpg', 1, 'formación'),
+    ('Paquete de fotografía submarina', 'Equipo completo para capturar impresionantes imágenes bajo el agua', 199.99, 249.99, 'https://www.saltedlineeurope.com/cdn/shop/products/1000X1000-Sony_c078659f-643f-428f-b2e0-c1e55b217129_800x.jpg?v=1649660413', 0, 'foto'),
+    ('Accesorios de buceo', 'Una amplia selección de accesorios para buceadores de todos los niveles', 29.99, 39.99, 'https://www.elblogdelbuzo.com/wp-content/uploads/2018/08/Equipo-de-buceo.jpg', 2, 'accesorio'),
+    ('Tour nocturno de buceo', 'Experiencia única de buceo nocturno para explorar la vida marina bajo la luz de la luna', 129.99, 179.99, 'https://cdn.getyourguide.com/img/tour/41bcebd1d23f7038531e818bcc66fba916bd1db60ea554105398826bba8f77c1.jpeg/145.jpg', 0, 'aventura'),
+    ('Botellas de oxígeno', 'Botellas de oxígeno de alta presión para buceo seguro y prolongado', 89.99, 109.99, 'https://www.hersill.com/wp-content/uploads/2018/11/Botellas-oxigeno.jpg', 2, 'equipo'),
+    ('Curso de rescate acuático', 'Aprende técnicas de rescate y primeros auxilios en el agua', 179.99, 229.99, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvSHXY0b0f6H6h29U3DToTgimDzLZy1jg7Ugk-ix8zjw&s', 1, 'formación'),
+    ('Viaje a islas tropicales', 'Paquete de vacaciones que incluye buceo en aguas cristalinas y playas paradisíacas', 799.99, 999.99, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhgS5XPDtHr3DI5Ge5PlkaRUgKisEdRxWtXhTViMJP7w&s', 0, 'vacaciones'),
+    ('Curso de fotografía submarina', 'Aprende a capturar la belleza del mundo submarino con este curso especializado', 149.99, 199.99, 'https://www.subacuaticasrealsociedad.com/upload/secciones-publicas/2021/06/08/subacuaticas-real-sociedad-curso-fotografia-submarina-iniciacion_grande.jpg', 1, 'formación'),
+    ('Guantes de buceo', 'Guantes de neopreno resistentes al agua para mayor protección y agarre', 39.99, 49.99, 'https://m.media-amazon.com/images/I/81keiDatV1L._AC_UF894,1000_QL80_.jpg', 2, 'equipo'),
+    ('Expedición a naufragios', 'Explora los misterios de naufragios históricos en esta emocionante expedición', 299.99, 349.99, 'https://www.lavanguardia.com/files/article_gallery_microformat/uploads/2024/03/20/65fad6cb74c01.jpeg', 0, 'aventura'),
+    ('Kit de reparación de equipos de buceo', 'Herramientas y materiales esenciales para la reparación de equipos de buceo', 79.99, 99.99, 'https://www.scubadivingfanclub.com/image-files/Diving_repair_kit_de_reparacion.jpg', 2, 'equipo'),
+    ('Bautismo de buceo', 'Prueba introductoria al mundo del buceo bajo la supervisión de un instructor certificado', 79.99, 99.99, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAr-DFD6A9meRShQLvlUUb3KvDMqrpKEJxbsbpMlUGpQ&s', 0, 'iniciación'),
+    ('Linterna submarina', 'Linterna resistente al agua para explorar cuevas y grietas submarinas', 29.99, 39.99, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlNyEZjDf79xldAWp2YfXiqw-6JB-Tr1fFalVNn33TjQ&s', 2, 'equipo'),
+    ('Curso de biología marina', 'Sumérgete en el fascinante mundo de la biología marina con este curso educativo', 199.99, 249.99, 'https://cdn.euroinnova.edu.es/img/subidasEditor/animales%20marinos-1597750093.jpg', 1, 'formación'),
+    ('Excursión a arrecifes de coral', 'Navega y explora arrecifes de coral llenos de vida marina en esta emocionante excursión', 119.99, 169.99, 'https://images.myguide-cdn.com/phuquoc/companies/phu-quoc-coral-reef-snorkeling-tour-with-sunset-dinner/large/phu-quoc-coral-reef-snorkeling-tour-with-sunset-dinner-2651092.jpg', 0, 'naturaleza'),
+    ('Tanque de buceo de aluminio', 'Tanque de aluminio ligero y duradero para inmersiones largas y seguras', 149.99, 199.99, 'https://m.media-amazon.com/images/I/61srKEE26eL._AC_UF894,1000_QL80_.jpg', 2, 'equipo'),
+    ('Tour de snorkel guiado', 'Descubre la belleza de los arrecifes de coral en un tour guiado de snorkel', 59.99, 79.99, 'https://media.tacdn.com/media/attractions-splice-spp-674x446/06/f0/fb/08.jpg', 0, 'aventura'),
+    ('Máscara de buceo panorámica', 'Máscara de buceo de diseño panorámico para una visión clara bajo el agua', 49.99, 69.99, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhFYuDeMw1mg1_5nwfw-kvvVhvDqGPM9oDQ7k9eXUQWw&s', 2, 'equipo'),
+    ('Cuchillo de buceo', 'Cuchillo de buceo de alta calidad para cortar cuerdas y realizar tareas submarinas', 69.99, 89.99, 'https://www.cascoantiguo.com/35342-large_default/cuchillo-micra-inox.jpg', 2, 'equipo'),
+    ('Tabla de mareas', 'Tabla de mareas resistente al agua para planificar tus inmersiones con precisión', 19.99, 29.99, 'https://media1.svb-marine.es/media/snr/508060/images/normal/img_2023-10-19_09-05-48_27a44f16e851889dbbf9538dc9e6e370.jpg', 2, 'equipo'),
+    ('Curso de navegación marina', 'Aprende técnicas de navegación marina para aventuras seguras en alta mar', 179.99, 229.99, 'https://formacion.camarabilbao.com/wp-content/uploads/sites/4/2023/12/curso-imbs-patron-navegacion-basica.jpg', 1, 'formación'),
+    ('Viaje de buceo en cuevas', 'Explora asombrosas cuevas submarinas en un emocionante viaje de buceo', 399.99, 499.99, 'https://www.a-alvarez.com/img/ybc_blog/post/maxresdefault-1.jpg', 0, 'aventura'),
+    ('Regulador de buceo', 'Regulador de buceo de alto rendimiento para una respiración suave y segura bajo el agua', 129.99, 159.99, 'https://www.snorkelybuceo.com/wp-content/uploads/2016/10/regulador-buceo-2.jpg', 2, 'equipo'),
+    ('Safari de buceo', 'Safari submarino para descubrir una variedad de especies marinas en su hábitat natural', 499.99, 599.99, 'https://www.buceoriasbaixas.com/wp-content/uploads/2020/09/Safari-con-caballitos-de-mar.jpg', 0, 'aventura');
 
 
 
 INSERT INTO item (id, weight, stock)
 VALUES
-    (2, 1.8, 10),
-    (5, 3, 20),
-    (8, 2.5, 5),
-    (10, 2, 2);
+    (2, 1.9, 0),
+    (5, 3.2, 25),
+    (8, 2.7, 8),
+    (10, 2.3, 0),
+    (14, 2.1, 3),
+    (16, 1.9, 6),
+    (18, 2.2, 7),
+    (21, 1.8, 9),
+    (23, 2.5, 12),
+    (24, 1.7, 0),
+    (25, 2.6, 14),
+    (28, 2.8, 0);
 
 
 
 INSERT INTO activity (id, level_required, time_starts, time_ends, available_spaces, available) VALUES
     (1, 0, '2024-03-21 10:00:00', '2024-03-21 12:00:00', 20, true),
-    (3, 0, '2024-03-21 10:00:00', '2024-03-21 12:00:00', 10, true),
-    (4, 0, '2024-03-21 10:00:00', '2024-03-21 12:00:00', 0, false),
-    (6, 0, '2024-03-21 10:00:00', '2024-03-21 12:00:00', 7, true),
+    (3, 2, '2024-03-21 10:00:00', '2024-03-21 12:00:00', 10, true),
+    (4, 1, '2024-03-21 10:00:00', '2024-03-21 12:00:00', 0, false),
+    (6, 2, '2024-03-21 10:00:00', '2024-03-21 12:00:00', 7, true),
     (7, 0, '2024-03-21 10:00:00', '2024-03-21 12:00:00', 2, true),
     (9, 0, '2024-03-21 10:00:00', '2024-03-21 12:00:00', 9, true),
-    (11, 2, '2024-03-22 14:00:00', '2024-03-22 16:00:00', 0, false),
-    (12, 2, '2024-03-22 14:00:00', '2024-03-22 16:00:00', 0, false);
+    (11, 1, '2024-03-22 14:00:00', '2024-03-22 16:00:00', 0, true),
+    (12, 1, '2024-03-22 14:00:00', '2024-03-22 16:00:00', 0, true),
+    (13, 2, '2024-03-22 14:00:00', '2024-03-22 16:00:00', 0, false),
+    (15, 0, '2024-03-22 14:00:00', '2024-03-22 16:00:00', 0, true),
+    (17, 0, '2024-03-22 14:00:00', '2024-03-22 16:00:00', 0, true),
+    (19, 1, '2024-03-22 14:00:00', '2024-03-22 16:00:00', 0, false),
+    (20, 1, '2024-03-22 14:00:00', '2024-03-22 16:00:00', 0, true),
+    (22, 0, '2024-03-22 14:00:00', '2024-03-22 16:00:00', 0, true),
+    (26, 0, '2024-03-22 14:00:00', '2024-03-22 16:00:00', 0, true),
+    (27, 2, '2024-03-22 14:00:00', '2024-03-22 16:00:00', 0, true),
+    (29, 1, '2024-03-22 14:00:00', '2024-03-22 16:00:00', 0, true);
 
 
 
