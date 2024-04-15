62fb6918e95ad972ac50296f074147895ce2e30f
David Tejerina <davidtejerina2003@gmail.com>
2024-04-15 16:07:34 +0200

    Incorporado Apartado Mensajes - data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -72,8 +72,8 @@ INSERT INTO activity (id, level_required, time_starts, time_ends, available_spac
 
 INSERT INTO user_ (nickname, password, name, surnames, email, phone, birthday, address, level, registration_date, last_login, role)
 VALUES
-    ('RashBack', '$2a$12$K4tojeaYWMK55KzWzDWtLOuuUjRTkycWhSGHYWA2LXMZqmZUtuXPO', 'David', 'Tejerina', 'a', 630156482, '2000-03-22 16:00:00', 'Getafe', 0, NOW(), NOW(), 0),
-    ('ElDon', '$2a$12$K4tojeaYWMK55KzWzDWtLOuuUjRTkycWhSGHYWA2LXMZqmZUtuXPO', 'Alex', 'Peña', 'b', 630926482, '1997-03-22 16:00:00', 'Pinto', 2, NOW(), NOW(), 0),
+    ('RashBack', '$2a$12$K4tojeaYWMK55KzWzDWtLOuuUjRTkycWhSGHYWA2LXMZqmZUtuXPO', 'David', 'Tejerina', 'davidtejerina2003@gmail.com', 630156482, '2000-03-22 16:00:00', 'Getafe', 0, NOW(), NOW(), 0),
+    ('ElDon', '$2a$12$K4tojeaYWMK55KzWzDWtLOuuUjRTkycWhSGHYWA2LXMZqmZUtuXPO', 'Alex', 'Peña', 'a', 630926482, '1997-03-22 16:00:00', 'Pinto', 2, NOW(), NOW(), 0),
     ('Administrador', '$2a$12$K4tojeaYWMK55KzWzDWtLOuuUjRTkycWhSGHYWA2LXMZqmZUtuXPO', 'Administrador', 'Administrador', 'admin', null, null, null, null, null, null, 1);
 
 
@@ -100,11 +100,11 @@ VALUES
 
 INSERT INTO Messages (email, content, date)
 VALUES
-    ('a', 'Contenido del mensaje 1', NOW()),
-    ('a', 'Contenido del mensaje 2', NOW()),
-    ('a', 'Contenido del mensaje 3', NOW()),
-    ('a', 'Contenido del mensaje 4', NOW()),
-    ('b', 'Contenido del mensaje 5', NOW()),
-    ('b', 'Contenido del mensaje 6', NOW()),
-    ('b', 'Contenido del mensaje 7', NOW()),
-    ('b', 'Contenido del mensaje 8', NOW());
+    ('a', '¡Hola! Espero que estés teniendo un excelente día. Recuerda tomarte un momento para relajarte y disfrutar de las pequeñas cosas. La vida está llena de sorpresas maravillosas. ¡Sonríe y sigue adelante con determinación!', NOW()-100),
+    ('a', 'Buenos días. Que este nuevo día te traiga nuevas oportunidades y momentos de felicidad. No te olvides de mantener una actitud positiva y enfrentar los desafíos con valentía. Recuerda que cada día es una nueva aventura.', NOW()-200),
+    ('a', '¿Cómo estás? Espero que te encuentres bien. Si estás pasando por un momento difícil, recuerda que siempre hay luz al final del túnel. Mantén la esperanza y busca el apoyo de tus seres queridos. ¡Tú puedes superarlo!', NOW()-100),
+    ('a', 'Hola amigo/a, ¿cómo va todo? Espero que estés disfrutando de tus actividades diarias. Recuerda que la vida está llena de altibajos, pero lo importante es mantener una actitud positiva y seguir adelante con determinación. ¡Ánimo!', NOW()-240),
+    ('a', 'Buenas tardes. Espero que estés teniendo un día increíble hasta ahora. Recuerda tomarte un momento para apreciar las cosas hermosas que te rodean. La vida está llena de pequeñas alegrías esperando ser descubiertas. ¡Disfruta cada momento!', NOW()-220),
+    ('davidtejerina2003@gmail.com', '¿Cómo estás hoy? Espero que estés bien. Si estás pasando por un momento difícil, recuerda que siempre hay personas dispuestas a ayudarte. No dudes en buscar el apoyo que necesitas. La vida es un viaje compartido.', NOW()-100),
+    ('b', 'Hola, ¿qué tal estás? Espero que estés disfrutando de un día maravilloso. No olvides detenerte de vez en cuando para apreciar las cosas simples de la vida. A veces, son esas pequeñas cosas las que hacen la mayor diferencia.', NOW()-200),
+    ('b', 'Buenas noches. Espero que hayas tenido un día lleno de momentos especiales. Recuerda que cada día es una oportunidad para crecer y aprender algo nuevo. Descansa bien esta noche y prepárate para un nuevo día lleno de posibilidades.', NOW());
