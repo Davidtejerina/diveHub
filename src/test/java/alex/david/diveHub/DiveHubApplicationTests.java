ac892b4c458c40054647a02b74752c36831a940a
Alex <alejandroplozano@gmail.com>
2024-05-17 13:14:51 +0200

    dockerFile Ok

--- src/test/java/alex/david/diveHub/DiveHubApplicationTests.java
+++ src/test/java/alex/david/diveHub/DiveHubApplicationTests.java
@@ -1,9 +1,10 @@
 package alex.david.diveHub;
 
+import alexDavid.DiveHubApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = DiveHubApplication.class)
 class DiveHubApplicationTests {
 
 	@Test
